17cb4710f004d349961bb89dfdc9f9d121245ca4
Breif7 <miguelmtzo1701@gmail.com>
2023-12-16 23:58:49 -0600

    Agregar pagina de inicio

---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido a la Página Principal</h1>
+      {/* Contenido de la página principal */}
+    </div>
+  );
+}
+
+export default HomePage;
